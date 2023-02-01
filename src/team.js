846103086a2846df26243964e767f35d21396d65
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2023-02-01 07:58:55 +0530

    Convert MembersImg to optimized .webp file and rename to team 2022

--- src/team.js
+++ src/team.js
@@ -1,175 +1,198 @@
-export const team =
-    [
-        {
-            id: 0,
-            department: "Organizing Leads",
-            members: [
-                {
-                    name: "Priyansh Tratiya",
-                    image: "images/MembersImg/Priyansh Tratiya.jpg",
-                    role: "Organiser",
-                    socials: ["https://www.instagram.com/priyansh.tratiya/", "https://twitter.com/TratiyaPriyansh"]
-                },
+export const team = [
+  {
+    id: 0,
+    department: "Organizing Leads",
+    members: [
+      {
+        name: "Priyansh Tratiya",
+        image: "images/Team 2022/Priyansh Tratiya.webp",
+        role: "Organiser",
+        socials: [
+          "https://www.instagram.com/priyansh.tratiya/",
+          "https://twitter.com/TratiyaPriyansh",
+        ],
+      },
 
-                {
-                    name: "Sachin Gupta",
-                    image: "images/MembersImg/Sachin Gupta.jpg",
-                    role: "Co-Organiser",
-                    socials: ["https://www.instagram.com/its.sachinnnnnn", "#"]
+      {
+        name: "Sachin Gupta",
+        image: "images/Team 2022/Sachin Gupta.webp",
+        role: "Co-Organiser",
+        socials: ["https://www.instagram.com/its.sachinnnnnn", "#"],
+      },
+    ],
+  },
+  {
+    id: 1,
+    department: "Curation Team",
+    members: [
+      {
+        name: "Chirantan Shah",
+        image: "images/Team 2022/Chirantan.webp",
+        role: "Curator In-chief",
+        socials: [
+          "https://www.instagram.com/_chir.antan_",
+          "https://twitter.com/Just_me4345",
+        ],
+      },
 
-                }]
+      {
+        name: "Anushka Jog",
+        image: "images/Team 2022/Anushka Jog.webp",
+        role: "Curator",
+        socials: [
+          "https://www.instagram.com/anushkajog/",
+          "https://twitter.com/AnushkaJog",
+        ],
+      },
 
-        },
-        {
-            id: 1,
-            department: "Curation Team",
-            members: [
-                {
-                    name: "Chirantan Shah",
-                    image: "images/MembersImg/Chirantan.jpg",
-                    role: "Curator In-chief",
-                    socials: ["https://www.instagram.com/_chir.antan_", "https://twitter.com/Just_me4345"]
-                },
+      {
+        name: "Maanya Shah",
+        image: "images/Team 2022/Maanya Shah.webp",
+        role: "Curator",
+        socials: ["#", "#"],
+      },
+    ],
+  },
+  {
+    id: 2,
+    department: "Design and Video Production Team",
+    members: [
+      {
+        name: "Prisha Rohan",
+        image: "images/Team 2022/PrishaRohan.webp",
+        role: "Lead Designer",
+        socials: [
+          "https://www.instagram.com/prisharohan/",
+          "https://twitter.com/prisha_rohan",
+        ],
+      },
 
-                {
-                    name: "Anushka Jog",
-                    image: "images/MembersImg/Anushka Jog.jpg",
-                    role: "Curator",
-                    socials: ["https://www.instagram.com/anushkajog/", "https://twitter.com/AnushkaJog"]
+      {
+        name: "Vidhi Patel",
+        image: "images/Team 2022/Vidhi Patel.webp",
+        role: "Lead Designer",
+        socials: ["https://www.instagram.com/vidhi._.127/", "#"],
+      },
 
-                },
+      {
+        name: "Aditya Khistawal",
+        image: "images/Team 2022/Aditya Kishtawal.webp",
+        role: "Video Production",
+        socials: [
+          "https://www.instagram.com/adidoz.k/",
+          "https://twitter.com/SastaNobita",
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    department: "Finance & Logistics Teams",
+    members: [
+      {
+        name: "Sanskar Shah",
+        image: "images/Team 2022/Sanskar Shah.webp",
+        role: "Budget Manager",
+        socials: [
+          "https://www.instagram.com/sansy.14/",
+          "https://twitter.com/SanskarShah16",
+        ],
+      },
 
-                {
-                    name: "Maanya Shah",
-                    image: "images/MembersImg/Maanya Shah.jpg",
-                    role: "Curator",
-                    socials: ["#", "#"]
+      {
+        name: "Harshita Kapri",
+        image: "images/Team 2022/Harshita Kapri.webp",
+        role: "Sponsership Manager",
+        socials: ["https://www.instagram.com/kapri_harshita/", "#"],
+      },
 
-                }]
-        },
-        {
-            id: 2,
-            department: "Design and Video Production Team",
-            members: [
-                {
-                    name: "Prisha Rohan",
-                    image: "images/MembersImg/PrishaRohan.jpg",
-                    role: "Lead Designer",
-                    socials: ["https://www.instagram.com/prisharohan/", "https://twitter.com/prisha_rohan"]
-                },
+      {
+        name: "Yogi Makadiya",
+        image: "images/Team 2022/YogiMakadiya.webp",
+        role: "Sponsership Manager",
+        socials: [
+          "https://www.instagram.com/yogi_my03/",
+          "https://twitter.com/YogiMakadiya",
+        ],
+      },
 
-                {
-                    name: "Vidhi Patel",
-                    image: "images/MembersImg/Vidhi Patel.jpg",
-                    role: "Lead Designer",
-                    socials: ["https://www.instagram.com/vidhi._.127/", "#"]
+      {
+        name: "Tirth Bhingradiya",
+        image: "images/Team 2022/Bhingradiya Tirth .webp",
+        role: "Logistics Manager",
+        socials: [
+          "https://www.instagram.com/tirth_bhingradiya_/",
+          "https://twitter.com/Tirth_6100",
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    department: "Event Management & Hospitality Team",
+    members: [
+      {
+        name: "Hitanshu Raulji",
+        image: "images/Team 2022/Hitanshu Raulji.webp",
+        role: "Event Manager",
+        socials: ["https://www.instagram.com/hitanshuraulji/", "#"],
+      },
 
-                },
+      {
+        name: "Krishi Godhani",
+        image: "images/Team 2022/KrishiGodhani.webp",
+        role: "Event Manager",
+        socials: ["https://www.instagram.com/krishi_2219/", "#"],
+      },
 
-                {
-                    name: "Aditya Khistawal",
-                    image: "images/MembersImg/Aditya Kishtawal.jpg",
-                    role: "Video Production",
-                    socials: ["https://www.instagram.com/adidoz.k/", "https://twitter.com/SastaNobita"]
+      {
+        name: "Madhuja Joshi",
+        image: "images/Team 2022/Madhuja Joshi_.webp",
+        role: "Event Manager",
+        socials: ["https://www.instagram.com/madhujaaa/", "#"],
+      },
 
-                }]
-        },
-        {
-            id: 3,
-            department: "Finance & Logistics Teams",
-            members: [
-                {
-                    name: "Sanskar Shah",
-                    image: "images/MembersImg/Sanskar Shah.jpg",
-                    role: "Budget Manager",
-                    socials: ["https://www.instagram.com/sansy.14/", "https://twitter.com/SanskarShah16"]
-                },
+      {
+        name: "Ana Dhamani",
+        image: "images/Team 2022/Ana.webp",
+        role: "Hospitality Manager",
+        socials: ["https://www.instagram.com/anadhamani/", "#"],
+      },
+    ],
+  },
+  {
+    id: 5,
+    department: "Publicity & Website Team",
+    members: [
+      {
+        name: "Dwija Modi",
+        image: "images/Team 2022/Dwija.webp",
+        role: "Publicity Manager",
+        socials: [
+          "https://www.instagram.com/dwija_13/",
+          "https://twitter.com/Dwija_13",
+        ],
+      },
 
-                {
-                    name: "Harshita Kapri",
-                    image: "images/MembersImg/Harshita Kapri.jpg",
-                    role: "Sponsership Manager",
-                    socials: ["https://www.instagram.com/kapri_harshita/", "#"]
+      {
+        name: "Atman Joshi",
+        image: "images/Team 2022/Atman 1.webp",
+        role: "Publicity Manager",
+        socials: [
+          "https://www.instagram.com/aatmanjoshi_29/",
+          "https://twitter.com/Atmanisreading",
+        ],
+      },
 
-                },
-
-                {
-                    name: "Yogi Makadiya",
-                    image: "images/MembersImg/YogiMakadiya.jpeg",
-                    role: "Sponsership Manager",
-                    socials: ["https://www.instagram.com/yogi_my03/", "https://twitter.com/YogiMakadiya"]
-
-                },
-
-                {
-                    name: "Tirth Bhingradiya",
-                    image: "images/MembersImg/Bhingradiya Tirth .jpg",
-                    role: "Logistics Manager",
-                    socials: ["https://www.instagram.com/tirth_bhingradiya_/", "https://twitter.com/Tirth_6100"]
-
-                }]
-        },
-        {
-            id: 4,
-            department: "Event Management & Hospitality Team",
-            members: [
-                {
-                    name: "Hitanshu Raulji",
-                    image: "images/MembersImg/Hitanshu Raulji.jpg",
-                    role: "Event Manager",
-                    socials: ["https://www.instagram.com/hitanshuraulji/", "#"]
-                },
-
-                {
-                    name: "Krishi Godhani",
-                    image: "images/MembersImg/KrishiGodhani.jpeg",
-                    role: "Event Manager",
-                    socials: ["https://www.instagram.com/krishi_2219/", "#"]
-
-                },
-
-                {
-                    name: "Madhuja Joshi",
-                    image: "images/MembersImg/Madhuja Joshi_.jpg",
-                    role: "Event Manager",
-                    socials: ["https://www.instagram.com/madhujaaa/", "#"]
-
-                },
-
-                {
-                    name: "Ana Dhamani",
-                    image: "images/MembersImg/Ana.jpeg",
-                    role: "Hospitality Manager",
-                    socials: ["https://www.instagram.com/anadhamani/", "#"]
-
-                }]
-        },
-        {
-            id: 5,
-            department: "Publicity & Website Team",
-            members: [
-                {
-                    name: "Dwija Modi",
-                    image: "images/MembersImg/Dwija.jpg",
-                    role: "Publicity Manager",
-                    socials: ["https://www.instagram.com/dwija_13/", "https://twitter.com/Dwija_13"]
-                },
-
-                {
-                    name: "Atman Joshi",
-                    image: "images/MembersImg/Atman 1.jpg",
-                    role: "Publicity Manager",
-                    socials: ["https://www.instagram.com/aatmanjoshi_29/", "https://twitter.com/Atmanisreading"]
-
-                },
-
-                {
-                    name: "Priyank Rana",
-                    image: "images/MembersImg/PriyankRana.jpg",
-                    role: "Website Manager",
-                    socials: ["https://www.instagram.com/em_priyank/", "https://twitter.com/em_Priyannk"]
-
-                }]
-        }
-
-    ];
+      {
+        name: "Priyank Rana",
+        image: "images/Team 2022/PriyankRana.webp",
+        role: "Website Manager",
+        socials: [
+          "https://www.instagram.com/em_priyank/",
+          "https://twitter.com/em_Priyannk",
+        ],
+      },
+    ],
+  },
+];
