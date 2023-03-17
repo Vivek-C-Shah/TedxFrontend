2720d9ca0afb9ee5b290d65829cd4657bccd32cd
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2023-03-17 17:05:27 +0530

    Add Sponsors Page

--- src/Pages/Sponsers.jsx
+++ src/Pages/Sponsers.jsx
@@ -1,10 +1,51 @@
 import React from "react";
-import Footer from "../Component/Footer";
-import Header2023 from "../Component/Header2023";
-import ScrollButton from "../Component/ScrollButton";
 import "./sponsers.css";
+import { sponsors } from "../sponsors";
+import ScrollButton from "../Component/ScrollButton";
+import { Container, Row, Col } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import Footer from "../Component/Footer";
+import AboutYearTab from "../Component/AboutYearTab/AboutYearTab";
+import Header2023 from "../Component/Header2023";
 
-const Sponsers = () => {
+
+const RenderMembers = (props) => {
+  return props.dept.members.map((member, i) => (
+    <Col key={i} lg={6} className="profile">
+      <img alt={member.name} src={member.image} className="profile-img" />
+      <h3 className="user-name">
+        <span>{member.name}</span>
+      </h3>
+      <h5 className="user-role">{member.description}</h5>
+
+      {/* <div className="member-social-media">
+        <a href={member.socials[0]} target="blank">
+          <FontAwesomeIcon size="sm" icon={faInstagram}></FontAwesomeIcon>
+        </a>
+        <a href={member.socials[1]} target="blank">
+          <FontAwesomeIcon size="sm" icon={faTwitter}></FontAwesomeIcon>
+        </a>
+      </div> */}
+    </Col>
+  ));
+};
+
+const RenderDepartment = () => {
+  return sponsors.map((dept, i) => (
+    <div key={i}>
+      <hr style={{ color: "red", height: "2px" }} />
+      <Container key={dept.id} className="pro">
+        <h2 className="title">{dept.department}</h2>
+        <Row className="profiles">
+          <RenderMembers dept={dept} />
+        </Row>
+      </Container>
+    </div>
+  ));
+};
+
+const Teams = () => {
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -16,8 +57,9 @@ const Sponsers = () => {
       {scrollToTop()}
       <div id="particles-js">
         <Header2023 />
-        <h1 className="heading-dis">Coming Soon!!!</h1>
-        <h1 className="heading-dis">Stay Tuned!!!</h1>
+        <div>
+          <RenderDepartment />
+        </div>
         <ScrollButton />
         <Footer />
       </div>
@@ -25,4 +67,4 @@ const Sponsers = () => {
   );
 };
 
-export default Sponsers;
+export default Teams;
---
+++ src/sponsors.js
@@ -0,0 +1,24 @@
+export const sponsors = [
+  {
+    id: 1,
+    department: "Refreshments Partner",
+    members: [
+      {
+        name: "Davat Bevarages",
+        image: "images/davat_sponsor.png",
+        description: "lorem150",
+      },
+    ],
+  },
+  {
+    id: 2,
+    department: "Visa Partner",
+    members: [
+      {
+        name: "Live Immigration",
+        image: "images/live_immigration_sponsor.jpg",
+        description: "lorem150",
+      },
+    ],
+  },
+];
