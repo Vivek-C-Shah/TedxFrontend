7848038f99daf2c3418ab92e8dab7d2023cad7b5
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2023-03-18 11:27:24 +0530

    Add Sponsors

--- src/Pages/Sponsers.jsx
+++ src/Pages/Sponsers.jsx
@@ -1,43 +1,63 @@
 import React from "react";
 import "./sponsers.css";
-import { sponsors } from "../sponsors";
+import { sponsors } from "../sponsors.js";
 import ScrollButton from "../Component/ScrollButton";
 import { Container, Row, Col } from "reactstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import Footer from "../Component/Footer";
-import AboutYearTab from "../Component/AboutYearTab/AboutYearTab";
 import Header2023 from "../Component/Header2023";
 
 
 const RenderMembers = (props) => {
-  return props.dept.members.map((member, i) => (
-    <Col key={i} lg={6} className="profile">
-      <img alt={member.name} src={member.image} className="profile-img" />
-      <h3 className="user-name">
-        <span>{member.name}</span>
-      </h3>
-      <h5 className="user-role">{member.description}</h5>
+  return (
+    <>
+      {
+        props.dept.members.map((member, i) => (
+    
+          <Col key={i} lg={6} className="sponsor__column">
 
-      {/* <div className="member-social-media">
-        <a href={member.socials[0]} target="blank">
-          <FontAwesomeIcon size="sm" icon={faInstagram}></FontAwesomeIcon>
-        </a>
-        <a href={member.socials[1]} target="blank">
-          <FontAwesomeIcon size="sm" icon={faTwitter}></FontAwesomeIcon>
-        </a>
-      </div> */}
-    </Col>
-  ));
+            <img className="sponsor__image" alt={member.name} src={member.image} />
+          <h3 className="sponsor__name">
+            <span>{member.name}</span>
+          </h3>
+          <h5 className="sponsor__description" id="description">
+            
+              {member.description}
+            
+          </h5>
+
+          {/* <a className="sponsor__link" href={member.link} target="_blank" rel="noopener noreferrer">
+            <p className="sponsor__link__text">{member.linkText}</p>
+          </a> */}
+          <a href={member.link} target="_blank" rel="noopener noreferrer">
+          <button className='payment__button'>{member.linkText}</button>
+          </a>
+
+          {/* <div>
+            <SponsorsDesc desc={member.description} />
+          </div> */}
+          {/* <div className="member-social-media">
+            <a href={member.socials[0]} target="blank">
+              <FontAwesomeIcon size="sm" icon={faInstagram}></FontAwesomeIcon>
+            </a>
+            <a href={member.socials[1]} target="blank">
+              <FontAwesomeIcon size="sm" icon={faTwitter}></FontAwesomeIcon>
+            </a>
+          </div> */}
+        </Col>
+           
+        ))
+      }
+    </>  
+  )
 };
 
 const RenderDepartment = () => {
   return sponsors.map((dept, i) => (
     <div key={i}>
       <hr style={{ color: "red", height: "2px" }} />
-      <Container key={dept.id} className="pro">
+      <Container className="sponsors__container" key={dept.id}>
         <h2 className="title">{dept.department}</h2>
-        <Row className="profiles">
+        <Row className="sponsors__row">
           <RenderMembers dept={dept} />
         </Row>
       </Container>
--- src/sponsors.js
+++ src/sponsors.js
@@ -5,8 +5,11 @@ export const sponsors = [
     members: [
       {
         name: "Davat Bevarages",
-        image: "images/davat_sponsor.png",
-        description: "lorem150",
+        image: "images/Sponsors 2023/davat_sponsor.png",
+        description:
+          "Davat Beverages is complete beverages company, bringing the best of traditional and contemaporary aspects of beverages. Offering more than a dozen brands. Their wide range of brands satisfy the requirements of diverse consumers across Gujarat, Maharashtra, Madhya Pradesh and Rajasthan.",
+        link: "https://www.davatbeverages.com/",
+        linkText: "Checkout more products →",
       },
     ],
   },
@@ -16,8 +19,11 @@ export const sponsors = [
     members: [
       {
         name: "Live Immigration",
-        image: "images/live_immigration_sponsor.jpg",
-        description: "lorem150",
+        image: "images/Sponsors 2023/live_immigration_sponsor.png",
+        description:
+          "Excited to announce that Live Immigration has joined forces with TEDxPDEU as our official Immigration Partner. From accommodation to your first part-time job, Live immigration makes the overseas visa process feel seamless!",
+        link: "https://www.instagram.com/global_pathway_services/",
+        linkText: "Find more details →",
       },
     ],
   },
